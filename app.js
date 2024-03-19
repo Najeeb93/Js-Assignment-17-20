4ad11aa2a5fd20472dd145afafe0928099e253cc
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-03-19 20:52:45 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -4,6 +4,15 @@
 
 //--Question No: 1
 
-for(i=0; i<=5; i++){
-    document.write("Hello world" + "<br>")
+// for(i=0; i<=5; i++){
+//     document.write("Hello world" + "<br>")
+// }
+
+
+//--Question No: 2
+
+for(i=1; i<=10; i++){
+
+    document.write(i + "<br>")
 }
+
