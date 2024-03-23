a758a4c60dbfb8fc783c34de5c3fbf4872b8d42e
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-03-23 21:20:24 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -144,9 +144,9 @@
 //     }
 // }
 
-for(var i = 1; i <= 100; i++){
-    console.log(i);
-    for(var j = 1; j <= 10; j++){
-        console.log(`${i} * ${j} = ${i*j}`);
-    }
-}
+// for(var i = 1; i <= 100; i++){
+//     console.log(i);
+//     for(var j = 1; j <= 10; j++){
+//         console.log(`${i} * ${j} = ${i*j}`);
+//     }
+// }
