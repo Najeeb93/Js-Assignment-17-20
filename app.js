d11b6fbb67c9c27e51c8cc0f0729e20db2e429db
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-03-28 20:04:14 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -10,15 +10,15 @@
     // }
     
     
-    //--Question No: 2
-    
     // var arr = [[1,2,3],[4,5,6],[7,8,9]];
     
     // for(var i =0; i < arr.length; i++){
-    //     document.write(arr[i].join(" ") + "<br>");
-    // }
-
-// for(i=1; i<=10; i++){
+        //     document.write(arr[i].join(" ") + "<br>");
+        // }
+        
+        //--Question No: 2
+        
+        // for(i=1; i<=10; i++){
 
 //     document.write(i + "<br>")
 // }
